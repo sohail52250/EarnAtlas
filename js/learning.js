eba92eebe53fe74acc945eba1f9544d982ccd735
sohail52250 <it03346543200@gmail.com>
2026-07-19 12:55:14 +0500

    Add bilingual learning engine and business advertising

---
+++ js/learning.js
@@ -0,0 +1,27 @@
+﻿let progress = JSON.parse(localStorage.getItem("earnatlas_progress")) || {};
+
+function completeLesson(id){
+
+progress[id] = "completed";
+
+localStorage.setItem(
+"earnatlas_progress",
+JSON.stringify(progress)
+);
+
+alert("Lesson Completed");
+
+location.reload();
+
+}
+
+
+function checkUnlock(previous){
+
+if(progress[previous]=="completed"){
+return true;
+}
+
+return false;
+
+}
