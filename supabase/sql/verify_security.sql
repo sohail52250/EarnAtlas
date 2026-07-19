952a9d8bda1ed3db037b98dbc8810ba849603f2b
sohail52250 <it03346543200@gmail.com>
2026-07-20 00:15:26 +0500

    Add database security verification query

---
+++ supabase/sql/verify_security.sql
@@ -0,0 +1,9 @@
+﻿-- EarnAtlas Security Verification
+
+select tablename, rowsecurity
+from pg_tables
+where schemaname = 'public';
+
+select schemaname, tablename, policyname
+from pg_policies
+where schemaname = 'public';
