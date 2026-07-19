61d5b3d25ec9c911590bc0b481fce211d463bf0e
sohail52250 <it03346543200@gmail.com>
2026-07-19 12:57:30 +0500

    Add bilingual user accounts and student dashboard foundation

---
+++ js/account.js
@@ -0,0 +1,51 @@
+﻿let user =
+JSON.parse(localStorage.getItem("earnatlas_user"))
+||
+{
+ name:"Student",
+ email:"",
+ progress:[]
+};
+
+
+function register(){
+
+let name=document.getElementById("name").value;
+let email=document.getElementById("email").value;
+
+
+user.name=name;
+user.email=email;
+
+
+localStorage.setItem(
+"earnatlas_user",
+JSON.stringify(user)
+);
+
+
+alert("Account created");
+
+window.location.href="../dashboard/";
+
+}
+
+
+
+function showUser(){
+
+let box=document.getElementById("profile");
+
+if(box){
+
+box.innerHTML=
+"Welcome "+user.name+
+"<br>Email: "+user.email;
+
+}
+
+}
+
+
+window.onload=showUser;
+
