9eb701c9e4b7f67bedad0f67c1bf9b51d6be700e
sohail52250 <it03346543200@gmail.com>
2026-07-19 13:10:12 +0500

    Add Supabase anon key

--- supabase/js/client.js
+++ supabase/js/client.js
@@ -5,7 +5,7 @@
 const SUPABASE_URL = "https://tawpzlyywnnpafobqumo.supabase.co";
 
 
-const SUPABASE_KEY = "PASTE_YOUR_ANON_KEY_HERE";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhd3B6bHl5d25ucGFmb2JxdW1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0NDE1NjYsImV4cCI6MjEwMDAxNzU2Nn0.PR5EdV8F2Ebn0NxUqQ3kFgEg1FLxzrZ1fQhM_U-Mqmg";
 
 
 export const supabase =
@@ -13,4 +13,5 @@ createClient(
 SUPABASE_URL,
 SUPABASE_KEY
 );
+
 
