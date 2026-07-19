e987c782f33fe07266184733119863a30d86b5ee
sohail52250 <it03346543200@gmail.com>
2026-07-19 05:02:58 +0500

    Add EarnAtlas user authentication interface

---
+++ js/auth.js
@@ -0,0 +1,48 @@
+
+function registerUser(){
+
+let user={
+
+name:document.getElementById('name').value,
+
+email:document.getElementById('email').value
+
+};
+
+
+localStorage.setItem(
+'earnatlas_user',
+JSON.stringify(user)
+);
+
+
+alert('Account created successfully');
+
+window.location.href='../dashboard/index.html';
+
+}
+
+
+
+function loginUser(){
+
+let user=
+localStorage.getItem('earnatlas_user');
+
+
+if(user){
+
+alert('Login successful');
+
+window.location.href='../dashboard/index.html';
+
+}
+
+else{
+
+alert('Please register first');
+
+}
+
+}
+
