1db3cbf61b83aef5b70ed3064c49d2ad49cf8242
sohail52250 <it03346543200@gmail.com>
2026-07-19 13:10:53 +0500

    Add EarnAtlas learning path

---
+++ learning/lessons.js
@@ -0,0 +1,47 @@
+﻿export const lessons = [
+
+{
+id:1,
+level:"Beginner",
+title_en:"Introduction to Online Earning",
+title_ur:"آن لائن کمائی کا تعارف",
+locked:false
+},
+
+
+{
+id:2,
+level:"Beginner",
+title_en:"Finding Your First Skill",
+title_ur:"اپنی پہلی مہارت تلاش کریں",
+locked:true
+},
+
+
+{
+id:3,
+level:"Beginner",
+title_en:"Creating Your Online Profile",
+title_ur:"اپنا آن لائن پروفائل بنائیں",
+locked:true
+},
+
+
+{
+id:4,
+level:"Intermediate",
+title_en:"Freelancing Platforms",
+title_ur:"فری لانسنگ پلیٹ فارم",
+locked:true
+},
+
+
+{
+id:5,
+level:"Advanced",
+title_en:"Building Digital Income",
+title_ur:"ڈیجیٹل آمدنی بنانا",
+locked:true
+}
+
+];
