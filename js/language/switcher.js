fd6d0477962760b1f972411a78001ce5d3648d85
sohail52250 <it03346543200@gmail.com>
2026-07-19 13:55:07 +0500

    Add bilingual English Urdu language switcher

---
+++ js/language/switcher.js
@@ -0,0 +1,55 @@
+﻿let language =
+localStorage.getItem("earnatlas_language") || "en";
+
+
+function setLanguage(lang){
+
+language=lang;
+
+localStorage.setItem(
+"earnatlas_language",
+lang
+);
+
+location.reload();
+
+}
+
+
+
+function translatePage(){
+
+
+document.querySelectorAll("[data-en]")
+.forEach(el=>{
+
+
+if(language==="ur"){
+
+el.innerText =
+el.getAttribute("data-ur");
+
+}
+
+else{
+
+el.innerText =
+el.getAttribute("data-en");
+
+}
+
+
+});
+
+
+}
+
+
+
+window.setLanguage=setLanguage;
+
+
+window.addEventListener(
+"DOMContentLoaded",
+translatePage
+);
