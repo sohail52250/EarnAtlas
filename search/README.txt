712324b88d889d40c281e6ba624b8b7742a406eb
sohail52250 <it03346543200@gmail.com>
2026-07-19 14:57:27 +0500

    Add search and discovery system

---
+++ search/README.txt
@@ -0,0 +1,20 @@
+﻿EarnAtlas Search System
+
+
+[X] Course search
+
+[X] Marketplace search
+
+[X] Skill discovery
+
+[X] Database indexes
+
+
+Future:
+
+[ ] AI search
+
+[ ] Voice search
+
+[ ] Personalized results
+
---
+++ supabase/sql/search.sql
@@ -0,0 +1,16 @@
+﻿create index if not exists lessons_title_search_idx
+
+on lessons(title_en);
+
+
+
+create index if not exists marketplace_title_search_idx
+
+on marketplace_items(title);
+
+
+
+create index if not exists marketplace_category_search_idx
+
+on marketplace_items(category);
+
