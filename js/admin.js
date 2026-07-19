54967d3e866cc4c76be1642caffd5e68cada9c9f
sohail52250 <it03346543200@gmail.com>
2026-07-19 12:58:15 +0500

    Add bilingual admin panel and content management foundation

---
+++ js/admin.js
@@ -0,0 +1,64 @@
+﻿let lessons =
+JSON.parse(localStorage.getItem("earnatlas_lessons"))
+|| [];
+
+
+function addLesson(){
+
+let title =
+document.getElementById("title").value;
+
+
+let description =
+document.getElementById("description").value;
+
+
+lessons.push(
+{
+title:title,
+description:description
+}
+);
+
+
+localStorage.setItem(
+"earnatlas_lessons",
+JSON.stringify(lessons)
+);
+
+
+alert("Lesson Added");
+
+location.reload();
+
+}
+
+
+
+function showLessons(){
+
+let box=document.getElementById("lessonList");
+
+
+if(box){
+
+box.innerHTML="";
+
+
+lessons.forEach(function(item){
+
+box.innerHTML +=
+"<div class='card'>"+
+"<h3>"+item.title+"</h3>"+
+"<p>"+item.description+"</p>"+
+"</div>";
+
+});
+
+}
+
+}
+
+
+window.onload=showLessons;
+
