5c44d7edc530eb7e274fffa182c8047d749f0885
sohail52250 <it03346543200@gmail.com>
2026-07-19 12:56:33 +0500

    Add bilingual lessons quizzes progress and certificates

---
+++ js/academy.js
@@ -0,0 +1,46 @@
+﻿let userProgress =
+JSON.parse(localStorage.getItem("earnatlas_progress"))
+|| {};
+
+
+function passLesson(id){
+
+userProgress[id]="passed";
+
+localStorage.setItem(
+"earnatlas_progress",
+JSON.stringify(userProgress)
+);
+
+alert("Congratulations! Lesson passed.");
+
+window.location.href="../";
+
+}
+
+
+
+function unlock(id){
+
+return userProgress[id]=="passed";
+
+}
+
+
+
+function quizCheck(answer,lesson){
+
+if(answer=="correct"){
+
+passLesson(lesson);
+
+}
+
+else{
+
+alert("Try again. Review the lesson.");
+
+}
+
+}
+
